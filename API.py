207ef0fecc54846e3a9993b3404c567d4428066d
Cristina-Acosta <criis.acosta.94@gmail.com>
2025-10-30 08:59:52 -0300

    Update API.py
    CREANDO CSV

--- API.py
+++ API.py
@@ -1,46 +1,45 @@
 import requests
-#URL ANTES DEL "?fields"
-URL="https://restcountries.com/v3.1/name/"
+import csv
+URL="https://restcountries.com/v3.1/all?fields=name,population,area,continents"
 
-def get_pais(nombre):
-    #url para obtener datos según requiera el usuario
-    url = f"{URL}{nombre}?fields=name,population,area,continents"
+def paises_lista():
     #GET para obtener información de la API y cargarla en respuesta
-    respuesta = requests.get(url, timeout=5)
+    respuesta = requests.get(URL, timeout=5)
+    print (respuesta)
     #Comprobando que se ejecute con éxito
     if respuesta.status_code == 200:
         #generando un json de respuesta e ingresando a la lista datos
         datos = respuesta.json()
-        print(datos)
-        print(datos[0])
-        if datos:
-            #Guardamos en pais el diccionario de la lista datos
-            pais = datos[0]
-            #Retornamos la información esencal
-            return{
-                "nombre": pais["name"]["common"],
-                "poblacion": pais["population"],
-                "superficie": pais["area"],
-                "continente": pais["continents"][0]
-            }
-        else:
-            print("No se encontraron datos para este país.")
+        paises_=[]
+        for i in range(len(datos)):
+            pais= datos[i]
+            paises={"nombre":pais["name"]["common"],
+            "poblacion":pais["population"],
+            "area":pais["area"],
+            "continente":pais["continents"][0]}
+            paises_.append(paises)
+        return paises_#RETORNA LISTA DE FILTRADA POR EL FOR
     else:
         print("Error al obtener datos:", respuesta.status_code)
 
-#Probando pantalla estética con la información
-if __name__ == "__main__":
-    def mostrar_datos_pais(info):
-        print("\nInformación del país:")
-        print(f"País: {info["nombre"]}")
-        print(f"Continente: {info["continente"]}")
-        print(f"Población: {info["poblacion"]} habitantes")  
-        print(f"Superficie: {info["superficie"]} km²")
-        print("-" * 50) 
-    nombre = input("Ingrese el nombre de un país: ").strip()
-    mostrar_datos_pais(get_pais(nombre))
+def paises_csv(lista_paises):
+    paises=lista_paises
+#INGRESAMOS LA LISTA DE PAÍSES PARA CREAR EL CSV
+    with open("C:\\Users\\f\\OneDrive\\Escritorio\\integrador\\prueba2\\paises.csv", "w", newline="", encoding="utf-8") as archivo:
+        #ESCRIBIMOS ENCABEZADOS
+        escritor = csv.DictWriter(archivo, fieldnames=["Nombre", "Población", "Area", "Continente"])
+        
+        escritor.writeheader()  
+#INGRESAMOS LOS PAÍSES SEGÚN LOS ENCABEZADOS AL CSV
+        for pais in paises:
+            escritor.writerow({
+                "Nombre": pais["nombre"],
+                "Población": pais["poblacion"],
+                "Area": pais["area"],
+                "Continente": pais["continente"]
+            })
+    print("CSV CREADO")
 
 
-
-
-
+    paises_data=paises_lista()
+    paises_csv(paises_data)
